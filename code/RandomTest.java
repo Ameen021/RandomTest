ccbfd4d75875fa92ab5eb62358b871ccf539d064
Amin <157623913+Ameen021@users.noreply.github.com>
2024-02-13 20:58:49 -0500

    Add files via upload

---
+++ code/RandomTest.java
@@ -0,0 +1,12 @@
+/* 
+File Name : RandomTest.jav
+Author : Ameen SHW
+Date : 2/13/2024
+Discription : Print greetings and name. 
+*/
+
+public class RandomTest {
+    public static void main(String args[]) {
+        System.out.println("Hello" + '\n' + "Ameen");
+    }
+    }
