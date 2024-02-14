3ce59bc07c28fe119380bec035c0ecab2c4ddf12
Ameen021 <mohammadaminshadiyan27@gmail.com>
2024-02-13 22:42:56 -0500

    first push

--- code/RandomTest.java
+++ code/RandomTest.java
@@ -4,9 +4,14 @@ Author : Ameen SHW
 Date : 2/13/2024
 Discription : Print greetings and name. 
 */
-
+import java.util.Scanner;
 public class RandomTest {
     public static void main(String args[]) {
+       Scanner scn = new Scanner(System.in);
+       System.out.println("put the user name ");
+       String  UserName = scn.nextLine();
+       System.out.println("the user name is " + UserName);
         System.out.println("Hello" + '\n' + "Ameen");
+    scn.close();
     }
     }
